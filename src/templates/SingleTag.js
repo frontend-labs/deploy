54b4716e5512a545704124313e8896a3d9e2743d
Andrés Muñoz <anmunoz@belatrixsf.com>
2020-09-26 23:47:13 -0500

    chore: actualizando deployment de wercker a github actions
    Estos cambios están relacionados a mover la integración
    contínua de wercker a github actions
    
    Fixes #29

--- src/templates/SingleTag.js
+++ src/templates/SingleTag.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Layout from '../components/Layout';
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
 const SingleTagTemplate = ({ data, pageContext }) => {
-  const { posts, tagName } = pageContext;
-  return(
+  const { posts, tagName } = pageContext
+  return (
     <Layout>
       <h3>Posts acerca de {tagName}:</h3>
       <div>
         <ul>
           {posts.map((post, index) => (
             <li key={index}>
-              <Link to={post.frontmatter.path}>
-                {post.frontmatter.title}
-              </Link>
+              <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
             </li>
           ))}
         </ul>
@@ -22,4 +20,4 @@ const SingleTagTemplate = ({ data, pageContext }) => {
   )
 }
 
-export default SingleTagTemplate;
+export default SingleTagTemplate
